a377219609c4fb186c726e70efa172b52b13ed09
pballmer <peterb@dashwire.com>
2016-02-29 12:56:47 -0800

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,5 +1,16 @@
+var captions = {
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+	assets/stanford1.jpg = "Having a ton of fun at the Stanford bball game!",
+}
 $('.photo-tile').click(function(){
 	$("#photo-details-photo").attr('src', $($(this)[0]).attr("src"));
+	document.getElementById("caption-text").innerHTML = "New text!";
 	$(".photo-details").removeClass('photo-details-hidden');
 });
 
